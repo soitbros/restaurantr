beedef2530a373c542e27f42c3db97e154749733
Clint Osterholz <clintosterholz@gmail.com>
2015-12-21 12:09:36 -0500

    adds styling for server check

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,28 @@
 //= require_tree .
 
 console.log("say what")
+
+window.onload = init;
+
+function init(){
+
+  var selectName = $('select').attr('name');
+
+  // add a hidden element with the same name as the select
+  var hidden = $('<input type="hidden" name="'+selectName+'">');
+  hidden.val($('select').val());
+  hidden.insertAfter($('select'));
+
+  $("select option").unwrap().each(function() {
+      var btn = $('<div class="btn">'+$(this).text()+'</div>');
+      if($(this).is(':checked')) btn.addClass('on');
+      $(this).replaceWith(btn);
+  });
+
+  $(document).on('click', '.btn', function() {
+      $('.btn').removeClass('on');
+      $(this).addClass('on');
+      $('input[name="'+selectName+'"]').val($(this).text());
+  });
+
+};
